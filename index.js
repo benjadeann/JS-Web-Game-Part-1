80986b048b0f8422d64ceb9715c19e65e6313746
“[firstname <benbedwell37@gmail.com>
2023-08-29 21:35:29 -0500

    added tree png

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+
+let pineTree = document.createElement('img')
+pineTree.src = 'assets/pine-tree.png'
+greenCharacter.style.position = 'fixed'
+greenCharacter.style.left = '450px'
+greenCharacter.style.bottom = '200px'
+document.body.append(pineTree)
